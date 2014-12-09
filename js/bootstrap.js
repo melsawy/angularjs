47c76a60a53a2a490299f0a24f29ec179a2a02e8
ramy_selim <rs@shopbox.com>
2014-12-09 09:10:12 +0100

    home controller

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5,11 +5,13 @@ require.config({
         app : 'modules/custom/angularjs/js/app',
         angularBootstrap : 'libraries/ui-bootstrap-tpls-0.12.0.min',
         angularUi: 'libraries/angular-ui-router',
-        mainController: 'modules/custom/angularjs/controllers/mainController'
+        mainController: 'modules/custom/angularjs/controllers/mainController',
+        homeController: 'modules/custom/angularjs/controllers/homeController',
     },
     shim: {
         'angular' : {'exports' : 'angular'},
         'mainController' :['angular','app'],
+        'homeController': ['angular','app'],
         'angularBootstrap': {
             deps:['angular'],
             'exports':'angular.bootstrap'
@@ -30,6 +32,7 @@ require( [
     'app',
     '/sites/all/modules/custom/angularjs/js/route.js',
     'mainController',
+    'homeController'
 ],function (angular, app, routes) {
     angular.element(document).ready(function () {
         angular.bootstrap(document, ['myapp']);
